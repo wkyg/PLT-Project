00a8d55b9219e379f8ca3771bcb54e03afcaac12
KelvinDesktop <39939811+wkyg@users.noreply.github.com>
2021-11-17 16:40:09 +0800

    table

--- ShiftReduceParser.py
+++ ShiftReduceParser.py
@@ -14,9 +14,11 @@
 
 
 import sys
+from prettytable import PrettyTable
+
+output = PrettyTable(["stack", "input", "action"])
 
 class Parser:
-    
     def __init__(self, text):
         self.text = text
         self.stack = []
@@ -30,8 +32,7 @@ class Parser:
     def popandstuff(self, x):
         for i in range(x):
             self.stack.pop()
-            #print("\n")
-            #print("stackstring",self.stackString)
+
             if(self.stackString == 'quack'):
                 self.stack.append("<Duck_Quack>")
 
@@ -56,31 +57,43 @@ class Parser:
                 self.stack.clear()
                 
         self.stackString = ""
+
         for i in range(len(self.stack)):
             self.stackString = self.stackString + self.stack[i]
-        print("$" + self.stackString + "\t\t\t" + self.a + "$" + "\t\t\t", end='')
+
+        output.add_row([self.stackString + self.a, "", ""])
+        print(output)
+        #print("$" + self.stackString + "\t\t\t" + self.a + "$" + "\t\t\t", end='')
         # print("\n")
         # print("stack",self.stack)
 
     def number(self):
             self.stacklength = len(self.stack)
             if self.stack[self.stacklength - 1] == "quack":
-                print(self.ac + "<Duck_Quack>")
+                output.add_row(["", "", self.ac + "<Duck_Quack>"])
+                print(output)
+                #print(self.ac + "<Duck_Quack>")
                 self.popandstuff(1)
             elif self.stack[self.stacklength - 1] == "honk":
-                print(self.ac + "<Duck_Honk>")
-                self.popandstuff(1)            
+                output.add_row(["", "", self.ac + "<Duck_Honk>"])
+                print(output)
+                #print(self.ac + "<Duck_Honk>")
+                self.popandstuff(1)
             
     def twopart(self, first, last):
         self.stacklength = len(self.stack)
         if self.stacklength > 2 and self.stack[self.stacklength - 1] == last:
             if self.stack[self.stacklength - 2] == first:
                 if self.stack[self.stacklength - 3] != "<EXP>":
-                    print(self.ac + first + last)
+                    output.add_row([self.ac + first + last])
+                    print(output)
+                    #print(self.ac + first + last)
                     self.popandstuff(2)
         elif self.stacklength > 1 and self.stack[self.stacklength - 1] == last:
             if self.stack[self.stacklength - 2] == first:
-                print(self.ac + first + last)
+                output.add_row([self.ac + first + last])
+                print(output)
+                #print(self.ac + first + last)
                 self.popandstuff(2)
        
     def threepart(self, first, middle, last):
@@ -88,13 +101,15 @@ class Parser:
         if self.stacklength > 2 and self.stack[self.stacklength - 1] == last:
             if self.stack[self.stacklength - 2] == middle:
                 if self.stack[self.stacklength - 3] == first:
-                    print(self.ac + first + middle + last)
+                    output.add_row([self.ac + first + middle + last])
+                    print(output)
+                    #print(self.ac + first + middle + last)
                     self.popandstuff(3)
 
     def checkrules(self):
         self.number()
-        self.twopart("<Duck_Quack>","<Duck_Honk>")
-        self.twopart("<Duck_Honk>","<Duck_Quack>")
+        self.twopart("<Duck_Quack>", "<Duck_Honk>")
+        self.twopart("<Duck_Honk>", "<Duck_Quack>")
 
     def checkvalid(self):
         self.stacklength = len(self.stack)
@@ -126,7 +141,9 @@ class Parser:
             self.stackString = ""
 
             # Print action
-            print(self.action)
+            output.add_row(["","",self.action])
+            #print(self.action)
+            print(output)
 
             # Pushing into stack
             if (self.text != ""):
@@ -144,15 +161,19 @@ class Parser:
                 self.a = self.a + self.text[i + self.inputElement][0]
 
             # Print stack and input
-            print("$" + self.stackString + "\t\t\t" + self.a + "$" + "\t\t\t", end='')
+            output.add_row(["", self.stackString, ""])
+            print(output)
+            #print("$" + self.stackString + "\t\t\t" + self.a + "$" + "\t\t\t", end='')
 
             self.checkrules()
 
     def parser(self):
         for x in range(len(self.text) - self.inputElement):
             self.a = self.a + self.text[x + self.inputElement][0]
-        print("stack \t\t\t input \t\t\t action")
-        print("$" + self.a + "$" + "\t\t\t", end='')
+        #output = PrettyTable(["stack", "input", "action"])
+        output.add_row(["", self.a, ""])
+        print(output)
+        #print("$" + self.a + "$" + "\t\t\t", end='')
 
         # Main function for shift reduce parser
         self.maincheck()
@@ -162,9 +183,3 @@ class Parser:
 
         # Check if syntax is correct or not
         self.checkvalid()
-
-
-            
-
-
-
